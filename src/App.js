7a3d7e8d9c22c87fe346c3b68deb0fdaf48908e1
hjoo830 <hjoo830@naver.com>
2024-09-21 02:18:00 +0900

    ♻️ Refactor: Separate the to-do list component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
+import Lists from "./components/Lists";
 
 export default function App() {
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
-  };
-
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    setTodoData(newTodoData);
-  };
 
   const handleChange = (e) => {
     setValue(e.target.value);
@@ -44,37 +23,13 @@ export default function App() {
     setValue("");
   };
 
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(newTodoData);
-  };
-
   return (
     <div className="container">
       <div className="todoBlock">
         <div className="title">
-          <h1>할일 목록</h1>
+          <h1>할 일 목록</h1>
         </div>
-
-        {todoData.map((data) => (
-          <div style={getStyle(data.completed)} key={data.id}>
-            <input
-              type="checkbox"
-              onChange={() => handleCompleteChange(data.id)}
-              defaultChecked={false}
-            />
-            {data.title}
-            <button style={btnStyle} onClick={() => handleClick(data.id)}>
-              x
-            </button>
-          </div>
-        ))}
-
+        <Lists todoData={todoData} setTodoData={setTodoData} />
         <form style={{ display: "flex" }} onSubmit={handleSubmit}>
           <input
             type="text"
