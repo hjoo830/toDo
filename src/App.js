cccde7ffaa98ba8109dd1593dddfa9bd443dc29e
hjoo830 <hjoo830@naver.com>
2024-09-21 02:25:53 +0900

    ♻️ Refactor: Separate the form component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
 import Lists from "./components/Lists";
+import Form from "./components/Form";
 
 export default function App() {
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,22 +27,7 @@ export default function App() {
           <h1>할 일 목록</h1>
         </div>
         <Lists todoData={todoData} setTodoData={setTodoData} />
-        <form style={{ display: "flex" }} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{ flex: "10", padding: "5px" }}
-            placeholder="해야 할 일을 입력하세요."
-            value={value}
-            onChange={handleChange}
-          />
-          <input
-            type="submit"
-            value="입력"
-            className="btn"
-            style={{ flex: "1" }}
-          />
-        </form>
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
     </div>
   );
