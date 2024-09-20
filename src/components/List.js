59a1f6d794bf49e56b3e7487e33c4094c98b2428
hjoo830 <hjoo830@naver.com>
2024-09-21 05:51:35 +0900

    ✨ Feat: Add edit functionality to the to-do list

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = React.memo(
   ({
@@ -11,6 +11,9 @@ const List = React.memo(
     snapshot,
     handleClick,
   }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
+
     const handleCompleteChange = (id) => {
       let newTodoData = todoData.map((data) => {
         if (data.id === id) {
@@ -21,32 +24,93 @@ const List = React.memo(
       setTodoData(newTodoData);
     };
 
-    return (
-      <div
-        key={id}
-        {...provided.draggableProps}
-        ref={provided.innerRef}
-        {...provided.dragHandleProps}
-        className={`${
-          snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-        } flex items-center justify-between w-full px-4 py-2 my-2 text-gray-600 border rounded`}
-      >
-        <div className="items-center">
-          <input
-            type="checkbox"
-            onChange={() => handleCompleteChange(id)}
-            defaultChecked={false}
-            className="mr-2"
-          />
-          <span className={completed ? "line-through" : undefined}>
-            {title}
-          </span>
+    const handleEditChange = (e) => {
+      setEditedTitle(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) {
+          data.title = editedTitle;
+        }
+        return data;
+      });
+      setTodoData(newTodoData);
+      setIsEditing(false);
+    };
+
+    if (isEditing) {
+      return (
+        <div
+          className={
+            "flex items-center justify-between w-full px-4 py-2 my-2 text-gray-600 bg-gray-100 border rounded"
+          }
+        >
+          <div className="items-center">
+            <form onSubmit={handleSubmit}>
+              <input
+                value={editedTitle}
+                onChange={handleEditChange}
+                className="w-full px-3 py-2 mr-4 text-gray-500 rounded"
+              />
+            </form>
+          </div>
+          <div className="items-center">
+            <button
+              className="px-4 py-2 float-right"
+              onClick={() => setIsEditing(false)}
+            >
+              ❌
+            </button>
+            <button
+              onClick={handleSubmit}
+              className="px-4 py-2 float-right"
+              type="submit"
+            >
+              ✔️
+            </button>
+          </div>
         </div>
-        <div className="items-center">
-          <button onClick={() => handleClick(id)}>x</button>
+      );
+    } else {
+      return (
+        <div
+          key={id}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+          className={`${
+            snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+          } flex items-center justify-between w-full px-4 py-2 my-2 text-gray-600 border rounded`}
+        >
+          <div className="items-center">
+            <input
+              type="checkbox"
+              onChange={() => handleCompleteChange(id)}
+              defaultChecked={false}
+              className="mr-2"
+            />
+            <span className={completed ? "line-through" : undefined}>
+              {title}
+            </span>
+          </div>
+          <div className="items-center">
+            <button
+              className="px-4 py-2 float-right"
+              onClick={() => handleClick(id)}
+            >
+              ❌
+            </button>
+            <button
+              className="px-4 py-2 float-right rotate-90"
+              onClick={() => setIsEditing(true)}
+            >
+              ✏️
+            </button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 );
 
